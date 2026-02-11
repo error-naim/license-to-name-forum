c7ede1e17e17face28c78bb497cfb4a5ff8a5e3b
Samuel6311 <56605188+Samuel6311@users.noreply.github.com>
2026-02-11 17:39:45 -0600

    restructure

---
+++ loadComponents.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("components/topbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("topbar-container").innerHTML = data;
+        });
+});
