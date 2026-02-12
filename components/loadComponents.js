576573a59d62ac50a2f0f1f315b0d94e7f93baeb
Samuel6311 <56605188+Samuel6311@users.noreply.github.com>
2026-02-11 18:50:14 -0600

    Update loadComponents.js

--- components/loadComponents.js
+++ components/loadComponents.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("components/topbar.html")
+    fetch("../components/topbar.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("topbar-container").innerHTML = data;
