3baea7b3b7cf528ad7622be9c2c64c09b0850812
Samuel6311 <56605188+Samuel6311@users.noreply.github.com>
2026-02-12 18:03:05 -0600

    fixes

--- components/loadComponents.js
+++ components/loadComponents.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/components/topbar.html")
+    const scriptUrl = document.currentScript ? document.currentScript.src : "";
+    const topbarUrl = scriptUrl ? new URL("topbar.html", scriptUrl).toString() : "../components/topbar.html";
+
+    fetch(topbarUrl)
         .then(response => response.text())
         .then(data => {
             document.getElementById("topbar-container").innerHTML = data;
