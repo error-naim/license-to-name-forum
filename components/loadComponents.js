13bf0a6d2d31811657ab3eb6b68ae0bfa965d862
Samuel6311 <56605188+Samuel6311@users.noreply.github.com>
2026-02-12 18:30:27 -0600

    Update loadComponents.js

--- components/loadComponents.js
+++ components/loadComponents.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const scriptUrl = document.currentScript ? document.currentScript.src : "";
-    const topbarUrl = scriptUrl ? new URL("topbar.html", scriptUrl).toString() : "../components/topbar.html";
+    const loaderScript = Array.from(document.scripts).find(function (script) {
+        return script.src && script.src.indexOf("components/loadComponents.js") !== -1;
+    });
+    const scriptUrl = loaderScript ? loaderScript.src : "";
+    const topbarUrl = scriptUrl ? new URL("topbar.html", scriptUrl).toString() : "/components/topbar.html";
 
     fetch(topbarUrl)
         .then(response => response.text())
